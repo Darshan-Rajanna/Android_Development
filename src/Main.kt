94b7f5f927d3628d5b98601a6f738650b98c47d5
Darshan-Rajanna <darshanrajanna07@gmail.com>
2025-03-24 09:09:28 +0530

    data types usage in Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,14 @@
 fun main(){
+
     println("Hello world")
+
     var userName: String = "Darshan"
     println("My name is $userName this is my 1st code of Kotlin learning")
+
+    var age: Int = 23
+    println("My age is $age years")
+
+    var sex = 'M'
+    println("my gender is $sex")
+
 }
