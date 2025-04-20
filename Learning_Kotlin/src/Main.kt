fe2ffdab4a2ce91f356c237aff6591092d2bb4e2
Darshan-Rajanna <darshanrajanna07@gmail.com>
2025-04-20 22:23:55 +0530

    Basics of Kotlin

---
+++ Learning_Kotlin/src/Main.kt
@@ -0,0 +1,23 @@
+fun main() {
+    var name="Kotlin";
+    println("Hello world of  $name");
+
+    val numberOfAdults = "20"
+    val numberOfKids = "30"
+    val total = numberOfAdults + numberOfKids
+    println("The total party size is: $total")
+
+
+    println(add(3 , 2 ));
+
+    name = "changed"
+
+    val a = "Darshan"
+
+
+}
+fun add(a: Int, b: Int): Int{
+
+    return a+b;
+
+}
